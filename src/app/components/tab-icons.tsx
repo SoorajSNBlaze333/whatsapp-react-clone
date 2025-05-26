0e0f0aab8312cf89b0a89506658f7fa0e2760e7d
SoorajSNBlaze333 <soorajsnblaze333@gmail.com>
2025-05-26 13:02:39 -0500

    feat: added dynamic messages, type simulator

--- src/app/components/tab-icons.tsx
+++ src/app/components/tab-icons.tsx
@@ -3,6 +3,7 @@ import TooltipWrapper from "./tooltip-wrapper";
 import { useTab } from "../hooks/use-tab";
 import TabIcon from "./tab-icon";
 import { useProfile } from "../hooks/use-profile";
+import { GithubLogoIcon } from "@phosphor-icons/react";
 
 export default function TabIcons() {
   const {
@@ -38,6 +39,15 @@ export default function TabIcons() {
       </section>
       <section className="flex flex-col justify-between items-center gap-2 py-4">
         <hr className="px-4 w-full border-[1px] border-gray-500/65" />
+        <TooltipWrapper tab="Github">
+          <a
+            href="https://github.com/SoorajSNBlaze333/whatsapp-react-clone"
+            target="_blank"
+            className="bg-gray-200 p-1 rounded-full"
+          >
+            <GithubLogoIcon className="size-5" weight="fill" />
+          </a>
+        </TooltipWrapper>
         <TooltipWrapper
           selected={selectedTab === "settings"}
           onClick={() => selectTab("settings")}
